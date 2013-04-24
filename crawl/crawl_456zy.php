996dcaca30f725c08aa8b4eaf83f4020ea133c2b
evanhou <hjpking@gmail.com>
2013-04-24 14:53:25 +0800

    test

---
+++ crawl/crawl_456zy.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: evan
+ * Date: 13-4-23
+ * Time: 下午8:20
+ * To change this template use File | Settings | File Templates.
+ */
+set_time_limit(200);
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+require libfile('class/crawl');
+
+$config = array('dir' => '/data/chunse/');
+
+
+
+$limit = 20;
+$start = 1;
+$end   = 12400;
+
+$url = 'http://www.456zy.com/detail/?%d.html';
+
+//*
+for ($i = $start; $i < $end; $i = $i + $limit)
+{
+    $crawl = new crawl_tools($config);
+    //$crawl = new crawl_tools($config);
+    echo $i."\n";
+    echo $i+$limit."\n";
+    $urlArray = array();
+
+    for ($ii = $i; $ii < $i+$limit; $ii++)
+    {
+        //* 抓取漏抓的页面
+        $fileName = $config['dir'].intToPath($ii).'index.html';
+        if (file_exists(($fileName))) continue;
+
+	    $urlArray[$ii] = sprintf($url, $ii);
+    }
+    //echo '<pre>';print_r($urlArray);exit;continue;
+
+    //*抓取漏抓的页面
+    if (empty ($urlArray)) continue;
+
+
+    $crawl->crawlList($urlArray);
+    unset ($crawl);
+}
+//*/
+
